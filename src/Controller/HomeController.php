21eecb4a6dd3a160fb82b4c520bd9901baec8333
MercierTanguy <tanguy.mercier@efrei.net>
2024-11-12 14:39:57 +0100

    home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MenuController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/menu', name: 'app_menu')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('item.html.twig', [
-            'controller_name' => 'MenuController',
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
